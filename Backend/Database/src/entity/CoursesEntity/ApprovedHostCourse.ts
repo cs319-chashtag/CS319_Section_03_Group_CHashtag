7d611803f1bb60293830d147e102a14ed0bb264f
Emrehan Hoşver <100358130+emrehan61@users.noreply.github.com>
2022-12-15 13:46:53 +0300

    New branch

--- Backend/Database/src/entity/CoursesEntity/ApprovedHostCourse.ts
+++
@@ -1,8 +0,0 @@
-import { Entity, Column, PrimaryColumn, ChildEntity, OneToOne, JoinColumn } from "typeorm"
-import { School } from "../SchoolEntity/School"
-import { HostCourse } from "./HostCourse"
-
-@ChildEntity()
-export class ApprovedHostCourse extends HostCourse{
-
-}
--- Backend/Database/src/entity/UsersEntity/Admin.ts
+++
@@ -1,6 +0,0 @@
-import { Entity, Column, ChildEntity } from 'typeorm'
-import { User } from './User'
-
-@ChildEntity()
-export class Admin extends User {
-}
