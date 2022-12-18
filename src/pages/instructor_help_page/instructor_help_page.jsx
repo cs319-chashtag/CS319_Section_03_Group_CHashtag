4435245087af5f1972d2ed058fcd3dc7aefc592d
Tuğberk Dikmen <63511175+tugberkdikmen@users.noreply.github.com>
2022-12-19 00:05:21 +0300

    Add files via upload

---
+++ src/pages/instructor_help_page/instructor_help_page.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import InstructorHelpComponent from "../../components/instructor_help_component/instructor_help_component";
+
+const InstructorHelpPage = () => {
+    return <InstructorHelpComponent />;
+};
+
+export default InstructorHelpPage;
---
+++ src/pages/instructor_todo_page/instructor_todo_page.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import InstructorToDoComponent from "../../components/instructor_todo_component/instructor_todo_component";
+
+const InstructorToDoPage = () => {
+    return <InstructorToDoComponent />;
+};
+
+export default InstructorToDoPage;
